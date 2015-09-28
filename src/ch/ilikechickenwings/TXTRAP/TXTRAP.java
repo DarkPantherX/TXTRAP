6e4ce1a7960a567154cab1961cbde9974fd0501e
BuildTools <unconfigured@null.spigotmc.org>
2015-09-28 13:24:49 +0200

    Added new main for an easier start

---
+++ src/ch/ilikechickenwings/TXTRAP/TXTRAP.java
@@ -0,0 +1,12 @@
+package ch.ilikechickenwings.TXTRAP;
+
+import ch.ilikechickenwings.TXTRAP.Frames.MainFrame;
+
+public class TXTRAP {
+	public static void main(String[] args) {
+		new MainFrame();
+
+	}
+
+	
+}
