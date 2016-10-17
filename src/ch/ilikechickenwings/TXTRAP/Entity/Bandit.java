660f58cc917e38a3ff6676b6d205a4173848c976
tim <tim@tim-ubuntu>
2016-10-17 08:14:37 +0200

    Comitted first change in a long time

--- src/ch/ilikechickenwings/TXTRAP/Entity/Bandit.java
+++ src/ch/ilikechickenwings/TXTRAP/Entity/Bandit.java
@@ -5,6 +5,11 @@ import ch.ilikechickenwings.TXTRAP.Places.Place;
 
 public class Bandit extends Human{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public Bandit(float health, String name, City city, String responseLine1, Place place2) {
 		super(health, name, city, responseLine1, place2);
 		
